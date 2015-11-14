fd66aa93819e089de8539f68cbf97f9857cab34a
Karol Wawrzecki <kwawrzecki@gmail.com>
2015-11-14 21:52:25 +0100

    trawis9

---
+++ src/UserBundle/Entity/Users.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * Users
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Users extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="google_id", type="string", nullable=true)
+     */
+    protected $googleId;
+
+}
+
